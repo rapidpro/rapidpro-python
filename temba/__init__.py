aba37cdf66c704e464e664e83e96ed480a5134e1
Rowan Seymour <rowanseymour@gmail.com>
2015-05-05 08:40:58 +0300

    Added labels_all param to get_messages

--- temba/__init__.py
+++ temba/__init__.py
@@ -247,7 +247,7 @@ class TembaClient(AbstractTembaClient):
         return Message.deserialize(self._get_single('messages', {'id': _id}))
 
     def get_messages(self, ids=None, broadcasts=None, urns=None, contacts=None, groups=None, statuses=None,
-                     direction=None, _types=None, labels=None, before=None, after=None, text=None,
+                     direction=None, _types=None, labels=None, labels_all=None, before=None, after=None, text=None,
                      archived=None, reverse=None, pager=None):
         """
         Gets all matching messages
@@ -260,7 +260,8 @@ class TembaClient(AbstractTembaClient):
         :param list[str] statuses: list of message statuses
         :param str direction: message direction (I or O)
         :param list[str] _types: list of message types
-        :param list[str] labels: list of message labels
+        :param list[str] labels: list of message labels (match ANY in list)
+        :param list[str] labels_all: list of message labels (match ALL in list)
         :param datetime before: created before
         :param datetime after: created after
         :param str text: containing text
@@ -269,8 +270,10 @@ class TembaClient(AbstractTembaClient):
         :param object pager: pager for paged results
         :return: list of messages
         """
-        params = self._build_params(id=ids, broadcast=broadcasts, urns=urns, contact=contacts, group_uuids=groups,
-                                    status=statuses, direction=direction, type=_types, label=labels,
+        params = self._build_params(id=ids, broadcast=broadcasts,
+                                    urns=urns, contact=contacts, group_uuids=groups,
+                                    status=statuses, direction=direction, type=_types,
+                                    label=labels, label_all=labels_all,
                                     before=before, after=after, text=text, archived=archived, reverse=reverse)
         return Message.deserialize_list(self._get_multiple('messages', params, pager))
 
