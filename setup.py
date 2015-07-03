1e20aac05e42ed573166fe26bcc7d4d4dd169c6a
Rowan Seymour <rowanseymour@gmail.com>
2015-07-03 14:26:49 +0100

    Updated setup.py according to pypi recommendations

--- setup.py
+++ setup.py
@@ -1,15 +1,33 @@
-#!/usr/bin/env python
+from os import path
+from setuptools import setup
 
-from distutils.core import setup
+here = path.abspath(path.dirname(__file__))
 
 setup(
     name='rapidpro-python',
     version='1.0',
     description='Python client library for the RapidPro',
-    author='Nyaruka',
     url='https://github.com/rapidpro',
+
+    author='Nyaruka',
+    author_email='code@nyaruka.com',
+
+    license='BSD',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    keywords='rapidpro client',
     packages=['temba'],
     install_requires=['pytz', 'requests'],
+
     test_suite='nose.collector',
     tests_require=['nose', 'mock'],
 )
