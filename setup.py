26e7176bce997410ba9a3caddde0b7aefa6fb67a
Norbert Kwizera <norkans7@gmail.com>
2015-09-02 18:53:32 +0200

    release version 1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='rapidpro-python',
-    version='1.1',
+    version='1.2',
     description='Python client library for the RapidPro',
     url='https://github.com/rapidpro',
 
