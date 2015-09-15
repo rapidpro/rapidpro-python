fd0f44ca379bf940c1cfbeda9490c9b3f40386b9
Norbert Kwizera <norkans7@gmail.com>
2015-09-15 23:31:18 +0200

    update version number to 1.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='rapidpro-python',
-    version='1.2',
+    version='1.3',
     description='Python client library for the RapidPro',
     url='https://github.com/rapidpro',
 
